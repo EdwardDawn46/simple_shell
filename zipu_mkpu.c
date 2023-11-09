3e0ace0ff2b71a5084ed63d66d30fea2298b6c6f
Benjamin <krazy2cub@gmail.com>
2023-11-09 02:23:57 -0800

    Execute Command

---
+++ zipu_mkpu.c
@@ -0,0 +1,39 @@
+#include "myAlxShell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * zipu_mkpu - Execute a shell command in a child process.
+ * @mkpu: The full path to the command to execute.
+ */
+
+void zipu_mkpu(const char *mkpu)
+{
+	pid_t pid;
+	char *args[2];
+
+	args[0] = (char *)mkpu;
+	args[1] = NULL;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(1);
+	} else if (pid == 0)
+	{
+		if (execve(args[0], args, NULL) == -1)
+		{
+			perror("execve");
+			exit(1);
+		}
+	}
+	else
+	{
+		wait(&status);
+	}
+}
