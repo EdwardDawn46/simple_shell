a2e0164068d073662bb016f8bde250227305f719
Benjamin <krazy2cub@gmail.com>
2023-11-09 02:04:18 -0800

    Header file

---
+++ myAlxShell.h
@@ -0,0 +1,11 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stddef.h>
+
+void gosi_mkpali(void);
+void de(const char *ozi);
+char *guo_ihedere(char *mkpu, size_t size);
+void zipu_mkpu(const char *mkpu);
+
+#endif
