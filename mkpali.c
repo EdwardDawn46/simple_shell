69267371b5be5f563d2ba46a98a7fdfbeb1a42d0
Benjamin <krazy2cub@gmail.com>
2023-11-09 02:34:04 -0800

    Prompt

---
+++ mkpali.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "myAlxShell.h"
+
+/**
+ * gosi_mkpali - Display a prompt for the user to enter a command.
+ */
+
+void gosi_mkpali(void)
+{
+	de("Hello user, how can i help you? ");
+}
